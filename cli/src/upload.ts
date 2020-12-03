f3255c80b4f62a224cb63ecb22fb8a5597491fac
Jeremy Weinstein <jeremydw@gmail.com>
2020-12-03 09:44:57 -0800

    Add progress bar for uploads

--- cli/src/upload.ts
+++ cli/src/upload.ts
@@ -1,5 +1,6 @@
+import * as cliProgress from 'cli-progress';
 import { mapLimit } from 'async';
-import {Manifest} from './manifest';
+import { Manifest } from './manifest';
 
 const {Storage} = require('@google-cloud/storage');
 
@@ -19,11 +20,16 @@ export interface Metadata {
 
 export async function uploadManifest(manifest: Manifest) {
   const storage = new Storage();
+
+  const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
   const numFiles = manifest.files.length;
+  let numProcessed = 0;
+  bar.start(numFiles, numProcessed);
+
   mapLimit(manifest.files, NUM_CONCURRENT_UPLOADS, (item, callback) => {
     let manifestFile = item;
     let remotePath = getFilePath(siteId, manifestFile.hash);
-    console.log(`Uploading ${manifestFile.cleanPath} -> ${bucket}/${remotePath}`);
+    // console.log(`Uploading ${manifestFile.cleanPath} -> ${bucket}/${remotePath}`);
     let metadata: Metadata = {
       cacheControl: 'public, max-age=31536000',
       contentType: manifestFile.mimetype,
@@ -31,11 +37,17 @@ export async function uploadManifest(manifest: Manifest) {
         path: manifestFile.cleanPath
       },
     };
+
+    // TODO: Handle upload errors and retries.
     storage.bucket(bucket).upload(manifestFile.path, {
       destination: remotePath,
       gzip: true,
       metadata: metadata,
     }).then(() => {
+      bar.update(numProcessed += 1);
+      if (numProcessed == numFiles) {
+        bar.stop();
+      }
       callback();
     });
   })
