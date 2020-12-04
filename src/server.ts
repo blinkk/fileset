c162f38f0037131012bd8faebee6254fba42bccb
Jeremy Weinstein <jeremydw@gmail.com>
2020-12-04 10:58:29 -0800

    Update server

--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,20 @@ const server = httpProxy.createProxyServer();
 const downloadManifest = async (shortsha: string) => {
   const key = datastore.key(['Fileset2Manifest', shortsha]);
   const entity = await datastore.get(key);
+  if (!entity || !entity[0]) {
+    return;
+  }
   const paths = entity[0].paths;
   return paths;
+};
+
+const parseHostname = (hostname: string) => {
+  // TODO: Make this more robust;
+  const parts = hostname.split('-dot-');
+  return {
+    siteId: parts[0],
+    branchOrRef: parts[1].slice(0, 7),
+  };
 };
 
 export function createApp(siteId: string, shortsha: string, branch: string) {
@@ -24,24 +36,33 @@ export function createApp(siteId: string, shortsha: string, branch: string) {
 
   const app = express();
   app.all('/*', async (req: express.Request, res: express.Response) => {
+    const envFromHostname = parseHostname(req.hostname);
+    const requestSiteId = envFromHostname.siteId || siteId;
+    const requestSha = envFromHostname.branchOrRef || shortsha;
+    const requestBranch = envFromHostname.branchOrRef || branch;
+    console.log(
+      `Handling request -> ${requestSiteId} @ ${requestBranch} (${requestBranch})`
+    );
+
     let blobPath = decodeURIComponent(req.path);
     if (blobPath.endsWith('/')) {
       blobPath += 'index.html';
     }
-
     shortsha = shortsha.slice(0, 7);
-    const manifest = await downloadManifest(shortsha);
-
+    const manifest = await downloadManifest(requestSha);
+    if (!manifest) {
+      res.sendStatus(404);
+      return;
+    }
     const blobKey = manifest[blobPath];
     const updatedUrl = blobKey
-      ? `/wing-prod.appspot.com/fileset/sites/${siteId}/blobs/${blobKey}`
+      ? `/wing-prod.appspot.com/fileset/sites/${requestSiteId}/blobs/${blobKey}`
       : '/404';
 
     if (!process.env.GAE_APPLICATION) {
       console.log(`Mapped ${req.path} -> ${blobPath} -> ${URL}${updatedUrl}`);
     }
     req.url = updatedUrl;
-
     // Add Authorization: Bearer ... header to outgoing GCS request.
     const client = await auth.getClient();
     const headers = await client.getRequestHeaders();
@@ -51,7 +72,6 @@ export function createApp(siteId: string, shortsha: string, branch: string) {
       changeOrigin: true,
       preserveHeaderKeyCase: true,
     });
-
     // Ensure responses sent to user are not cached (despite server-to-server responses being entirely cached as the blob keys do not change).
     server.on('proxyRes', (proxyRes, req, res) => {
       proxyRes.headers['Cache-Control'] = 'private, max-age=86400';
