af81dc5c98d362f5f509d58bb11a9ca8078ecc35
Jeremy Weinstein <jeremydw@gmail.com>
2021-02-22 09:35:56 -0800

    Add async support to top command

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
 #!/usr/bin/env node
 
-import {createCommand} from 'commander';
 import {ServeCommand} from './commands/serve';
 import {UploadCommand} from './commands/upload';
+import {createCommand} from 'commander';
 
-const program = createCommand();
+async function main() {
+  const program = createCommand();
 
-program
-  .command('upload [dir]')
-  .description('Uploads a directory to cloud storage')
-  .option('-s, --site <site>', 'site', '')
-  .option('-r, --ref <ref>', 'ref', '')
-  .option('-f, --force', 'force', false)
-  .option('-t, --ttl <ttl>', 'ttl', undefined)
-  .action((path, options) => {
-    const cmd = new UploadCommand(options);
-    cmd.run(path);
-  });
+  program
+    .command('upload [dir]')
+    .description('Uploads a directory to cloud storage')
+    .option('-s, --site <site>', 'site', '')
+    .option('-r, --ref <ref>', 'ref', '')
+    .option('-f, --force', 'force', false)
+    .option('-t, --ttl <ttl>', 'ttl', undefined)
+    .action(async (path, options) => {
+      const cmd = new UploadCommand(options);
+      await cmd.run(path);
+    });
 
-program
-  .command('serve')
-  .description('Runs the server')
-  .option('-s, --site <site>', 'site', '')
-  .option('-r, --ref <ref>', 'ref', '')
-  .action(options => {
-    const cmd = new ServeCommand(options);
-    cmd.run();
-  });
+  program
+    .command('serve')
+    .description('Runs the server')
+    .option('-s, --site <site>', 'site', '')
+    .option('-r, --ref <ref>', 'ref', '')
+    .action(options => {
+      const cmd = new ServeCommand(options);
+      cmd.run();
+    });
 
-program.parse(process.argv);
+  await program.parseAsync(process.argv);
+}
+
+main();
