bcbbf5c7b101b535f75036f7fdaf01a1c186f238
Jeremy Weinstein <jeremydw@gmail.com>
2021-09-21 07:56:32 -0700

    fix: exception handling

--- src/index.ts
+++ src/index.ts
@@ -4,35 +4,31 @@ import {ServeCommand} from './commands/serve';
 import {UploadCommand} from './commands/upload';
 import {createCommand} from 'commander';
 
-async function main() {
-  const program = createCommand();
+const program = createCommand();
 
-  program
-    .command('upload [dir]')
-    .description('Uploads a directory to cloud storage')
-    .option('-p, --google-cloud-project <project>', 'project', '')
-    .option('-s, --site <site>', 'site', '')
-    .option('-r, --ref <ref>', 'ref', '')
-    .option('-b, --branch <branch>', 'branch', '')
-    .option('-f, --force', 'force', false)
-    .option('-t, --ttl <ttl>', 'ttl', undefined)
-    .option('-o, --output-file <path>', 'output file', '')
-    .action(async (path, options) => {
-      const cmd = new UploadCommand(options);
-      await cmd.run(path);
-    });
+program
+  .command('upload [dir]')
+  .description('Uploads a directory to cloud storage')
+  .option('-p, --google-cloud-project <project>', 'project', '')
+  .option('-s, --site <site>', 'site', '')
+  .option('-r, --ref <ref>', 'ref', '')
+  .option('-b, --branch <branch>', 'branch', '')
+  .option('-f, --force', 'force', false)
+  .option('-t, --ttl <ttl>', 'ttl', undefined)
+  .option('-o, --output-file <path>', 'output file', '')
+  .action(async (path, options) => {
+    const cmd = new UploadCommand(options);
+    await cmd.run(path);
+  });
 
-  program
-    .command('serve')
-    .description('Runs the server')
-    .option('-s, --site <site>', 'site', '')
-    .option('-r, --ref <ref>', 'ref', '')
-    .action(options => {
-      const cmd = new ServeCommand(options);
-      cmd.run();
-    });
+program
+  .command('serve')
+  .description('Runs the server')
+  .option('-s, --site <site>', 'site', '')
+  .option('-r, --ref <ref>', 'ref', '')
+  .action(options => {
+    const cmd = new ServeCommand(options);
+    cmd.run();
+  });
 
-  await program.parseAsync(process.argv);
-}
-
-main();
+program.parse(process.argv);
